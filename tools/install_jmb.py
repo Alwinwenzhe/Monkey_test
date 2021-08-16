27b91b4302836878a204131fd5533b2a320735ad
Alwinwenzhe <3065524859@qq.com>
2021-08-16 12:01:57 +0800

    0816增加自动安装姐妹邦工具

--- tools/install_jmb.py
+++ tools/install_jmb.py
@@ -1,6 +1,14 @@
 
 import os, time
 def get_dev():
+    print("""
+运行前提：
+    1、手机开发者模式已打开，
+    2、usb调试模式已打开，
+    3、usb连接模式为文件传输
+    4、个别手机需要授权usb进行引用安装
+    5、个别手机可能会涉及传输完成后，手机点击安装
+    """)
     rt = os.popen('adb devices').readlines()
     rt = rt[1:-1]
     for i in rt:
